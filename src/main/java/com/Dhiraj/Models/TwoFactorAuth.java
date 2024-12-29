5d13f420bb14ae934aa1b5dd1896a8902a3755cb
Dhiraj <dk194945@gmail.com>
2024-12-29 20:20:46 +0530

    Add @Embeddable annotation to TwoFactorAuth model for JPA integration

--- src/main/java/com/Dhiraj/Models/TwoFactorAuth.java
+++ src/main/java/com/Dhiraj/Models/TwoFactorAuth.java
@@ -2,9 +2,11 @@ package com.Dhiraj.Models;
 
 import com.Dhiraj.Domain.VerificationType;
 
+import jakarta.persistence.Embeddable;
 import lombok.Data;
 
 @Data
+@Embeddable
 public class TwoFactorAuth {
     private boolean isEnabled = false;
     private VerificationType sendTo;
