57b378409db8ef4d0e0c20c921cded4e1d76dbf0
Dhiraj <dk194945@gmail.com>
2024-12-29 22:45:11 +0530

    Change ForgotPasswordRepository to use String as the ID type for ForgotPasswordToken

--- src/main/java/com/Dhiraj/Repository/ForgotPasswordRepository.java
+++ src/main/java/com/Dhiraj/Repository/ForgotPasswordRepository.java
@@ -3,6 +3,6 @@ package com.Dhiraj.Repository;
 import com.Dhiraj.Models.ForgotPasswordToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ForgotPasswordRepository extends JpaRepository<ForgotPasswordToken,Long> {
+public interface ForgotPasswordRepository extends JpaRepository<ForgotPasswordToken,String> {
     ForgotPasswordToken findByUserId(Long userId);
 }
