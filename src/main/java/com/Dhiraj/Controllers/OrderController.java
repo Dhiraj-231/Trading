af73a8b6545b0e3226fd15d82a02c602bfa7d0f4
Dhiraj <dk194945@gmail.com>
2025-01-13 11:58:03 +0530

    Add OrderController for managing user orders and payments

---
+++ src/main/java/com/Dhiraj/Controllers/OrderController.java
@@ -0,0 +1,84 @@
+package com.Dhiraj.Controllers;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Dhiraj.Models.Coin;
+import com.Dhiraj.Models.Order;
+import com.Dhiraj.Models.User;
+import com.Dhiraj.Request.CreateOrderRequest;
+import com.Dhiraj.Service.CoinService;
+import com.Dhiraj.Service.OrderService;
+import com.Dhiraj.Service.UserService;
+import com.Dhiraj.Service.WalletTransactionService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/orders")
+@RequiredArgsConstructor
+public class OrderController {
+    private final OrderService orderService;
+    private final CoinService coinService;
+    private final UserService userSerivce;
+    private final WalletTransactionService walletTransactionService;
+
+    @PostMapping("/pay")
+    public ResponseEntity<Order> payOrderPayment(
+            @RequestHeader("Authorization") String jwt,
+            @RequestBody CreateOrderRequest req
+
+    ) throws Exception {
+        User user = userSerivce.findUserProfileByJwt(jwt);
+        Coin coin = coinService.findById(req.getCoinId());
+
+        Order order = orderService.processOrder(coin, req.getQuantity(), req.getOrderType(), user);
+
+        return ResponseEntity.ok(order);
+
+    }
+
+    @GetMapping("/{orderId}")
+    public ResponseEntity<Order> getOrderById(
+            @RequestHeader("Authorization") String jwtToken,
+            @PathVariable Long orderId) throws Exception {
+        if (jwtToken == null) {
+            throw new Exception("token missing...");
+        }
+
+        User user = userSerivce.findUserProfileByJwt(jwtToken);
+
+        Order order = orderService.getOrderById(orderId);
+        if (order.getUser().getId().equals(user.getId())) {
+            return ResponseEntity.ok(order);
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<Order>> getAllOrdersForUser(
+            @RequestHeader("Authorization") String jwtToken,
+            @RequestParam(required = false) String order_type,
+            @RequestParam(required = false) String asset_symbol) throws Exception {
+        if (jwtToken == null) {
+            throw new Exception("token missing...");
+        }
+
+        Long userId = userSerivce.findUserProfileByJwt(jwtToken).getId();
+
+        List<Order> userOrders = orderService.getAllOrdersForUser(userId, order_type, asset_symbol);
+        return ResponseEntity.ok(userOrders);
+    }
+
+}
