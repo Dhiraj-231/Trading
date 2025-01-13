a73fe7304ee8b9dabbc1f98ff247a1b6b79dcc34
Dhiraj <dk194945@gmail.com>
2025-01-13 12:00:47 +0530

    Add PaymentController for handling payment processing and order creation

---
+++ src/main/java/com/Dhiraj/Controllers/PaymentController.java
@@ -0,0 +1,51 @@
+package com.Dhiraj.Controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Dhiraj.Domain.PaymentMethod;
+import com.Dhiraj.Exception.UserException;
+import com.Dhiraj.Models.PaymentOrder;
+import com.Dhiraj.Models.User;
+import com.Dhiraj.Response.PaymentResponse;
+import com.Dhiraj.Service.PaymentService;
+import com.Dhiraj.Service.UserService;
+import com.razorpay.RazorpayException;
+import com.stripe.exception.StripeException;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/payment")
+public class PaymentController {
+    private final UserService userService;
+    private final PaymentService paymentService;
+
+    @PostMapping("/api/payment/{paymentMethod}/amount/{amount}")
+    public ResponseEntity<PaymentResponse> paymentHandler(
+            @PathVariable PaymentMethod paymentMethod,
+            @PathVariable Long amount,
+            @RequestHeader("Authorization") String jwt) throws UserException, RazorpayException, StripeException {
+
+        User user = userService.findUserProfileByJwt(jwt);
+
+        PaymentResponse paymentResponse;
+
+        PaymentOrder order = paymentService.createOrder(user, amount, paymentMethod);
+
+        if (paymentMethod.equals(PaymentMethod.RAZORPAY)) {
+            paymentResponse = paymentService.createRazorpayPaymentLink(user, amount,
+                    order.getId());
+        } else {
+            paymentResponse = paymentService.createStripePaymentLink(user, amount, order.getId());
+        }
+
+        return new ResponseEntity<>(paymentResponse, HttpStatus.CREATED);
+    }
+}
