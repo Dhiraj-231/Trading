755e863232c25f2427dabd5917f136b29a1c2ccc
Dhiraj <dk194945@gmail.com>
2025-01-15 19:08:48 +0530

    Refactor PaymentServiceImp to improve code readability and update Stripe API key variable name

--- src/main/java/com/Dhiraj/Controllers/AuthController.java
+++ src/main/java/com/Dhiraj/Controllers/AuthController.java
@@ -1,19 +1,202 @@
 package com.Dhiraj.Controllers;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.Dhiraj.Config.JwtProvider;
+import com.Dhiraj.Exception.UserException;
+import com.Dhiraj.Models.TwoFactorOTP;
+import com.Dhiraj.Models.User;
 import com.Dhiraj.Repository.UserRepository;
+import com.Dhiraj.Request.LoginRequest;
+import com.Dhiraj.Response.AuthResponse;
+import com.Dhiraj.Service.EmailService;
+import com.Dhiraj.Service.TwoFactorOtpService;
 import com.Dhiraj.Service.UserService;
+import com.Dhiraj.Service.VerificationService;
+import com.Dhiraj.Service.WalletService;
+import com.Dhiraj.Service.WatchlistService;
+import com.Dhiraj.Service.ServiceImp.CustomeUserServiceImp;
+import com.Dhiraj.Utils.OtpUtils;
 
+import jakarta.mail.MessagingException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
+
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final CustomeUserServiceImp customUserDetails;
     private final UserService userService;
+    private final WatchlistService watchlistService;
+    private final WalletService walletService;
+    private final VerificationService verificationService;
+    private final TwoFactorOtpService twoFactorOtpService;
+    private final EmailService emailService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<AuthResponse> createUserHandler(
+            @RequestBody User user) throws UserException {
+
+        String email = user.getEmail();
+        String password = user.getPassword();
+        String fullName = user.getFullName();
+        String mobile = user.getMobile();
+
+        User isEmailExist = userRepository.findByEmail(email);
+
+        if (isEmailExist != null) {
+
+            throw new UserException("Email Is Already Used With Another Account");
+        }
+
+        // Create new user
+        User createdUser = new User();
+        createdUser.setEmail(email);
+        createdUser.setFullName(fullName);
+        createdUser.setMobile(mobile);
+        createdUser.setPassword(passwordEncoder.encode(password));
+
+        User savedUser = userRepository.save(createdUser);
+
+        watchlistService.createWatchList(savedUser);
+        // walletService.createWallet(user);
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(email, password);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String token = JwtProvider.generateToken(authentication);
+
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.setJwt(token);
+        authResponse.setMessage("Register Success");
+
+        return new ResponseEntity<AuthResponse>(authResponse, HttpStatus.OK);
+
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<AuthResponse> signing(@RequestBody LoginRequest loginRequest)
+            throws UserException, MessagingException {
+
+        String username = loginRequest.getEmail();
+        String password = loginRequest.getPassword();
+
+        System.out.println(username + " ----- " + password);
+
+        Authentication authentication = authenticate(username, password);
+
+        User user = userService.findUserByEmail(username);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String token = JwtProvider.generateToken(authentication);
+
+        if (user.getTwoFactorAuth().isEnabled()) {
+            AuthResponse authResponse = new AuthResponse();
+            authResponse.setMessage("Two factor authentication enabled");
+            authResponse.setTwoFactorAuthEnabled(true);
+
+            String otp = OtpUtils.generateOTP();
+
+            TwoFactorOTP oldTwoFactorOTP = twoFactorOtpService.findByUserId(user.getId());
+            if (oldTwoFactorOTP != null) {
+                twoFactorOtpService.deleteTwoFactorOtp(oldTwoFactorOTP);
+            }
+
+            TwoFactorOTP twoFactorOTP = twoFactorOtpService.createTwoFactorOTP(user, otp, token);
+
+            emailService.sendVerificationOtpEmail(user.getEmail(), otp);
+
+            authResponse.setSession(twoFactorOTP.getId());
+            return new ResponseEntity<>(authResponse, HttpStatus.OK);
+        }
+
+        AuthResponse authResponse = new AuthResponse();
+
+        authResponse.setMessage("Login Success");
+        authResponse.setJwt(token);
+
+        return new ResponseEntity<>(authResponse, HttpStatus.OK);
+    }
+
+    private Authentication authenticate(String username, String password) {
+        UserDetails userDetails = customUserDetails.loadUserByUsername(username);
+
+        System.out.println("sign in userDetails - " + userDetails);
+
+        if (userDetails == null) {
+            System.out.println("sign in userDetails - null " + userDetails);
+            throw new BadCredentialsException("Invalid username or password");
+        }
+        if (!passwordEncoder.matches(password, userDetails.getPassword())) {
+            System.out.println("sign in userDetails - password not match " + userDetails);
+            throw new BadCredentialsException("Invalid username or password");
+        }
+        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+    }
+
+    @GetMapping("/login/google")
+    public void redirectToGoogle(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+        // Redirect to the Google OAuth2 authorization URI
+        response.sendRedirect("/login/oauth2/authorization/google");
+    }
+
+    // /login/oauth2/code/google
+    @GetMapping("/login/oauth2/code/google")
+    public User handleGoogleCallback(@RequestParam(required = false, name = "code") String code,
+            @RequestParam(required = false, name = "state") String state,
+            OAuth2AuthenticationToken authentication) {
+
+        // Extract user details from the authentication object or access token
+        String email = authentication.getPrincipal().getAttribute("email");
+        String fullName = authentication.getPrincipal().getAttribute("name");
+        // You can extract more details as needed
+
+        User user = new User();
+        user.setEmail(email);
+        user.setFullName(fullName);
+
+        return user;
+    }
+
+    @PostMapping("/two-factor/otp/{otp}")
+    public ResponseEntity<AuthResponse> verifySigningOtp(
+            @PathVariable String otp,
+            @RequestParam String id) throws Exception {
+
+        TwoFactorOTP twoFactorOTP = twoFactorOtpService.findById(id);
+
+        if (twoFactorOtpService.verifyTwoFactorOtp(twoFactorOTP, otp)) {
+            AuthResponse authResponse = new AuthResponse();
+            authResponse.setMessage("Two factor authentication verified");
+            authResponse.setTwoFactorAuthEnabled(true);
+            authResponse.setJwt(twoFactorOTP.getJwt());
+            return new ResponseEntity<>(authResponse, HttpStatus.OK);
+        }
+        throw new Exception("invalid otp");
+    }
 }
--- src/main/java/com/Dhiraj/Service/ServiceImp/PaymentServiceImp.java
+++ src/main/java/com/Dhiraj/Service/ServiceImp/PaymentServiceImp.java
@@ -25,7 +25,7 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class PaymentServiceImp implements PaymentService {
-    @Value("${stripe.api.key}")
+    @Value("${stripe.secret.key}")
     private String stripeSecretKey;
 
     @Value("${razorpay.api.key}")
@@ -35,9 +35,11 @@ public class PaymentServiceImp implements PaymentService {
     private String apiSecret;
 
     private final PaymentOrderRepository paymentOrderRepository;
+
     @Override
     public PaymentOrder createOrder(User user, Long amount, PaymentMethod paymentMethod) {
-        PaymentOrder order=new PaymentOrder();
+
+        PaymentOrder order = new PaymentOrder();
         order.setUser(user);
         order.setAmount(amount);
         order.setPaymentMethod(paymentMethod);
@@ -46,24 +48,24 @@ public class PaymentServiceImp implements PaymentService {
 
     @Override
     public PaymentOrder getPaymentOrderById(Long id) throws Exception {
-        Optional<PaymentOrder> optionalPaymentOrder=paymentOrderRepository.findById(id);
-        if(optionalPaymentOrder.isEmpty()){
-            throw new Exception("payment order not found with id "+id);
+        Optional<PaymentOrder> optionalPaymentOrder = paymentOrderRepository.findById(id);
+        if (optionalPaymentOrder.isEmpty()) {
+            throw new Exception("payment order not found with id " + id);
         }
         return optionalPaymentOrder.get();
     }
 
     @Override
     public Boolean processPaymentOrder(PaymentOrder paymentOrder, String paymentId) throws RazorpayException {
-        if(paymentOrder.getStatus().equals(PaymentOrderStatus.PENDING)){
+        if (paymentOrder.getStatus().equals(PaymentOrderStatus.PENDING)) {
 
-            if(paymentOrder.getPaymentMethod().equals(PaymentMethod.RAZORPAY)){
+            if (paymentOrder.getPaymentMethod().equals(PaymentMethod.RAZORPAY)) {
                 RazorpayClient razorpay = new RazorpayClient(apiKey, apiSecret);
                 Payment payment = razorpay.payments.fetch(paymentId);
 
                 Integer amount = payment.get("amount");
                 String status = payment.get("status");
-                if(status.equals("captured")){
+                if (status.equals("captured")) {
                     paymentOrder.setStatus(PaymentOrderStatus.SUCCESS);
 
                     return true;
@@ -85,34 +87,32 @@ public class PaymentServiceImp implements PaymentService {
     public PaymentResponse createRazorpayPaymentLink(User user, Long Amount, Long orderId) throws RazorpayException {
         Long amount = Amount * 100;
 
-
         try {
             // Instantiate a Razorpay client with your key ID and secret
             RazorpayClient razorpay = new RazorpayClient(apiKey, apiSecret);
 
             JSONObject paymentLinkRequest = new JSONObject();
-            paymentLinkRequest.put("amount",amount);
-            paymentLinkRequest.put("currency","INR");
-
+            paymentLinkRequest.put("amount", amount);
+            paymentLinkRequest.put("currency", "INR");
 
             // Create a JSON object with the customer details
             JSONObject customer = new JSONObject();
-            customer.put("name",user.getFullName());
+            customer.put("name", user.getFullName());
 
-            customer.put("email",user.getEmail());
-            paymentLinkRequest.put("customer",customer);
+            customer.put("email", user.getEmail());
+            paymentLinkRequest.put("customer", customer);
 
             // Create a JSON object with the notification settings
             JSONObject notify = new JSONObject();
-            notify.put("email",true);
-            paymentLinkRequest.put("notify",notify);
+            notify.put("email", true);
+            paymentLinkRequest.put("notify", notify);
 
             // Set the reminder settings
-            paymentLinkRequest.put("reminder_enable",true);
+            paymentLinkRequest.put("reminder_enable", true);
 
             // Set the callback URL and method
-            paymentLinkRequest.put("callback_url","http://localhost:5173/wallet/"+orderId);
-            paymentLinkRequest.put("callback_method","get");
+            paymentLinkRequest.put("callback_url", "http://localhost:5173/wallet/" + orderId);
+            paymentLinkRequest.put("callback_method", "get");
 
             // Create the payment link using the paymentLink.create() method
             PaymentLink payment = razorpay.paymentLink.create(paymentLinkRequest);
@@ -120,9 +120,8 @@ public class PaymentServiceImp implements PaymentService {
             String paymentLinkId = payment.get("id");
             String paymentLinkUrl = payment.get("short_url");
 
-            PaymentResponse res=new PaymentResponse();
+            PaymentResponse res = new PaymentResponse();
             res.setPayment_url(paymentLinkUrl);
-
 
             return res;
 
@@ -138,23 +137,20 @@ public class PaymentServiceImp implements PaymentService {
         SessionCreateParams params = SessionCreateParams.builder()
                 .addPaymentMethodType(SessionCreateParams.PaymentMethodType.CARD)
                 .setMode(SessionCreateParams.Mode.PAYMENT)
-                .setSuccessUrl("http://localhost:5173/wallet?order_id="+orderId)
+                .setSuccessUrl("http://localhost:5173/wallet?order_id=" + orderId)
                 .setCancelUrl("http://localhost:5173/payment/cancel")
                 .addLineItem(SessionCreateParams.LineItem.builder()
                         .setQuantity(1L)
                         .setPriceData(SessionCreateParams.LineItem.PriceData.builder()
                                 .setCurrency("usd")
-                                .setUnitAmount(Amount*100)
-                                .setProductData(SessionCreateParams
-                                        .LineItem
-                                        .PriceData
-                                        .ProductData
+                                .setUnitAmount(Amount * 100)
+                                .setProductData(SessionCreateParams.LineItem.PriceData.ProductData
                                         .builder()
                                         .setName("Top up wallet")
-                                        .build()
-                                ).build()
-                        ).build()
-                ).build();
+                                        .build())
+                                .build())
+                        .build())
+                .build();
 
         Session session = Session.create(params);
 
